90fc760ab5b691e7590a3d525921531d4dc060ff
Erick Betancourt Garcia <erickbeta12@gmail.com>
2024-09-23 21:53:15 -0600

    initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+Este es mi intruduccion Mi nombre es Erick Betancourt Garcia y vivo en la ciudad de Torreón en el estado de Coahuila
+
---
+++ script.js
@@ -0,0 +1,29 @@
+  // Función para manejar el desplazamiento suave al hacer clic en enlaces
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substr(1); // Obtiene el ID de destino
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          behavior: 'smooth',
+          top: targetElement.offsetTop - 60, // Ajusta el desplazamiento si es necesario
+        });
+      }
+    });
+  });
+
+  
+  const menuHamburguesa = document.getElementById('menu-hamburguesa');
+  const navegacion = document.querySelector('ul.navegacion');
+  
+  menuHamburguesa.addEventListener('click', () => {
+    navegacion.classList.toggle('mostrar');
+  });
+  
+  document.getElementById('menu-hamburguesa').addEventListener('click', function () {
+    document.querySelector('.navegacion').classList.toggle('active');
+  });
